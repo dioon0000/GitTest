348d0f661c9785f77ae22d257363a5a1c336def0
DiooN <dioon0000@naver.com>
2025-09-02 12:24:25 +0900

    새 프로젝트 생성
    언리얼 프로젝트 생성
    소스트리를 활용한 커밋 푸시 방법 학습
    커밋 메세지의 제목과 내용은 한 줄 띄기를 통해 구분

---
+++ Source/GitTestProject/GitTestProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GitTestProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitTestProject, "GitTestProject" );
